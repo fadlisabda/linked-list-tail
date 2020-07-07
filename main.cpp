d5275ff1fa90a098cfbb05d0026c6cf6d99d0388
fadlisabda <fadlisabdad@yahoo.com>
2020-07-07 20:15:27 +0700

    menambahkan komentar,kode bahasa inggris dan bahasa indonesia

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//in Indonesian and in Indonesian
 //linked list tail
 #include <iostream>
 using namespace std;
@@ -222,3 +223,225 @@ int main()
       	        }
    	} while(pilih!='5');
 }
+
+//in English
+// #include <iostream>
+// using namespace std;
+
+// class node // class to create a node
+// {
+//     public:
+//       int value; // for values ​​in the node
+//       node * next; // object to go to the next node
+
+//    node (int va, node * n) // fill in the value and pointer
+//    {
+//    value = va;
+//       next = n;
+//    }
+
+//    node (int a) // fill in the value
+//    {
+//    value = a;
+//    }
+// };
+
+// dlistx class
+// {
+//       public:
+//       void inserthead (int val); // creates a node value at the head / front
+//       void removehead (); // delete the node on the head / front
+//       void insertlast (int val); // creates a node in the tail / back
+//       void removelast (); // removes the node on the tail / back
+//       void print (); // displays all nodes
+//       int isempty (); // returns 1 if an empty node returns 0 if the node contains
+
+//       dlistx () // fills null head and tail
+//       {
+//          head = NULL;
+//          tail = NULL;
+//       }
+
+//       private:
+//          node * head; // creates a private head and tail object
+//          node * tail;
+// };
+
+// static int list = 0; // counts the number of node values
+
+// int dlistx :: isempty () // returns the value 1 if the node is empty and 0 if the node contains
+// {
+// if (this-> head == NULL || this-> tail == NULL)
+//    return 1;
+//    else
+//    return 0;
+// }
+
+// void dlistx :: inserthead (int value) // creates a node value in the head section
+// {
+//    if (isempty () == 1) // if the node is still empty
+//    {
+//       node * temp;
+//       temp = new node (value); // creates a temp object
+//       this-> head = temp; // makes the object temp head
+//       this-> tail = temp; // makes the temp tail object
+//       this-> tail-> next = NULL; // makes the next tail node null
+//       cout << "Value" << value << "goes to head" << endl; // enters value to head
+//       cout << endl;
+//    } else // if the node already contains
+//    {
+//       node * temp; // creates a temp object
+//       temp = new node (value, this-> head); // temp stores the value and head object
+//       this-> head = temp; // temp is made head
+//       cout << "value" << value << "goes to head" << endl; // value goes to head
+//       cout << endl;
+//    }
+//    list ++; // the amount of data increases
+// }
+
+// void dlistx :: insertlast (int value) // creates a node in the tail
+// {
+//    if (isempty () == 1) // if the node is still empty
+//    {
+//       node * temp;
+//       temp = new node (value); // populates the temp value / value object
+//       this-> head = temp; // makes temp head
+//       this-> tail = temp; // makes temp tail
+//       this-> tail-> next = NULL; // the next tail node is null
+//       cout << "value" << value << "enter tail" << endl; // value enter tail
+//       cout << endl;
+//    } else // if the node contains
+//    {
+//       this-> tail-> next = new node (value); // the next new node is given a value
+//       this-> tail = this-> tail-> next; // new node is placed in front of the tail
+//       this-> tail-> next = NULL; // the next tail node is null
+//       cout << "value" << value << "enter tail" << endl; // value entered into tail
+//       cout << endl;
+//     }
+// list ++; // the amount of data increases
+// }
+
+
+// void dlistx :: removehead () // delete the node on the head / front
+// {
+//    if (isempty () == 1) // if the node is empty
+//         cout << "Sorry, the linked list is empty" << endl;
+//    else
+//         this-> head = this-> head-> next; // head is replaced by the node in front of it
+//    list --; // amount of data reduced
+// }
+
+
+// void dlistx :: removelast () // removes the node on the tail / back
+// {
+//    if (isempty () == 1) // if the node is still empty
+//    {
+//         cout << "Sorry, the linked list is empty" << endl;
+//    } else // if the node contains
+//    {
+//         int x = tail-> value; // populates the value of the tail node to variable x
+//         if (list == 1) // if the number of nodes is still 1
+//         {
+//      cout << "value" << x << "Has been deleted" << endl; // value has been deleted
+//      tail = NULL; // tail object 0
+//       head = NULL; // object head 0
+//            list --; // amount of data reduced
+//         } else // if the data is more than 1
+//         {
+//       node * temp; // creates a temp object
+//       temp = this-> head; // makes the object temp head
+//       for (int i = 1; i <list-1; i ++) // repeats until the tail
+//       {
+//       temp = temp-> next; // from the head node to the next node until the tail node
+//       }
+//       temp-> next = temp-> next-> next; // delete the value in the tail
+// this-> tail = temp; // previous tail value becomes a new one
+//       cout << "value" << x << "Has been deleted" << endl;
+//       list --; // amount of data reduced
+//        }
+//     }
+// }
+
+// void dlistx :: print () // displays all nodes
+// {
+//    node * newnode = head; // makes the newnode head object
+//    while (newnode! = NULL) // repeats until it is found null
+//    {
+//       cout << "Fill list:" <<newnode-> value << endl; // displays the node value
+//       newnode = newnode-> next; // goes to the next node from the head node
+//    }
+// }
+
+// int main ()
+// {
+//    int n;
+//    dlistx * st; // object to call the function
+//    st = new dlistx ();
+
+//    char choose;
+//    cout << "Linkedlist operation" << endl;
+//    cout << "1. insert" << endl;
+//    cout << "2. remove" << endl;
+//    cout << "3. insert last" << endl;
+//    cout << "4. remove last" << endl;
+//    cout << "5. Exit" << endl;
+   
+//    do {
+//    cout << endl;
+//    cout << "Options:";
+//         cin >> select;
+
+//             switch (select)
+//             {
+//     case '1':
+//                   cout << "enter data:";
+//                   cin >> n;
+//                   st-> inserthead (n);
+//                   st-> print ();
+//           break;
+
+//                   case '2':
+//          char n;
+//          cout << "Are you sure you want to delete? y / n";
+//                   cin >> n;
+//                   if (n == 'y')
+//                   {
+//                   st-> removehead ();
+//                   st-> print ();
+//                     } else
+//                     {
+//                   cout << "Thanks";
+//                     }
+//                   break;
+
+//                   case '3':
+//          int post;
+//                   cout << "enter data:";
+//                   cin >> post;
+//                   st-> insertlast (pos);
+//                   st-> print ();
+//           break;
+
+//           case '4':
+//          char k;
+//          cout << "Are you sure you want to delete? y / n";
+//                   cin >> k;
+//                   if (k == 'y')
+//                   {
+//                   st-> removelast ();
+//                         st-> print ();
+//                      } else
+//                      {
+//                   cout << "Thanks";
+//                      }
+//                        break;
+
+//            case '5':
+//          cout << "thank you:";
+//                     break;
+//                  default:
+//          cout << "wrong choice";
+//                break;
+//       }
+//    } while (select! = '5');
+// }
